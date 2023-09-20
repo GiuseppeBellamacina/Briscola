db812c5630ce201519752cf83f5021b863f9eab9
Giuseppe Bellamacina <giuseppe.bellamacina@studium.unict.it>
2023-09-20 13:58:23 +0200

    lib

---
+++ lib.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Installing necessary Python libraries...
+python -m pip install --upgrade pip
+python -m pip install os random pyfiglet termcolor colorama
+echo Libraries have been installed.
+pause
